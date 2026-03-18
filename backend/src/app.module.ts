c2571b9e41816247ae996cfdb2b497b0b68428e5
Hong-CHP <93changhong@gmail.com>
2026-03-18 19:09:19 +0100

    host word postgresql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    UsersModule, 
+    PrismaModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // 读取 .env
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
 	private pool:Pool;
 	constructor(){
+		console.log(process.env.DATABASE_URL);
 		const pool = new Pool({connectionString: process.env.DATABASE_URL});
 		const adapter = new PrismaPg(pool as any);
 		super({adapter});
