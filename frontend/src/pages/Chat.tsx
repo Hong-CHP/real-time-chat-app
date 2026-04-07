c840e872a79a2ce7145c7a84981c41bf9d1ff24b
Hong-CHP <93changhong@gmail.com>
2026-04-07 15:11:13 +0200

    add accept and refuse request, notifications and get messages service

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -18,24 +18,37 @@ export function getUserId() {
 	return decoded.sub
 }
 
-type message = {
+type Message = {
 	content: string;
 	senderId: string;
 	receiverId: string;
 }
 
+type FriendStatus = 'PENDING' | 'ACCEPTED' | 'REJECTED' | 'BLOCKED';
+
+type Notification = {
+	id: number;
+	fromUserId: number;
+	fromName: string;
+	status: FriendStatus;
+}
+
 function Chat () {
 	const [keyword, setKeyword] = useState<string>("")
 	const [searchResult, setSearchResult] = useState<any | null>(null)
-	const [messages, setMessages] = useState<message[]>([])
+	const [messages, setMessages] = useState<Message[]>([])
 	const [message, setMessage] = useState<string>("")
 	const [friends, setFriends] = useState<any[]>([])
 	const [receiverId, setReceiverId] = useState(-1)
 	const [socket, setSocket] = useState<any>(null)
+	const [openNotifications, setOpenNotifications] = useState(false)
+	const [notifications, setNotifications] = useState<Notification[]>([])
+	const [friendsVersion, setFriendsVersion] = useState(0)
 	
 	const token = localStorage.getItem("access_token")
 	const myId = getUserId()
 
+	// socket for receiveMessages, receive friend_request
 	useEffect(()=>{
 		const newSocket = createSocket()
 		setSocket(newSocket)
@@ -44,11 +57,27 @@ function Chat () {
 			setMessages(prev=>[...prev, data])
 		})
 
+		newSocket.on('friend_request', data=>{
+			setNotifications(prev=>[...prev, data])
+		})
+
 		return ()=>{
 			newSocket.disconnect()
 		}
 	}, [])
+	
+	// send a message
+	function handleSendMessage() {
+		if (!socket || receiverId === -1) return
 
+		socket.emit('sendMessage', {
+			content: message,
+			receiverId: receiverId,
+		})
+		setMessage("")
+	}
+	
+	// get friends list
 	useEffect(()=>{
 		fetch('/api/friend/list', {
 			method: "GET",
@@ -58,8 +87,9 @@ function Chat () {
 		})
 		.then(res=>res.json())
 		.then(data=>setFriends(data))
-	}, [])
+	}, [friendsVersion])
 
+	// find someone in users
 	async function handleSearchRequest(e: any) {
 		e.preventDefault()
 		const res = await fetch(`/api/users/search?keyword=${keyword}`, {
@@ -74,9 +104,8 @@ function Chat () {
 		setSearchResult(data)
 	}
 
-	async function handleAddFriend(friendId: number) {
-		console.log(typeof friendId, friendId)
-		
+	// sendRequest for adding a friend
+	async function handleAddFriend(friendId: number) {		
 		const res = await fetch('/api/friend/request', {
 				method: 'POST',
 				headers: {
@@ -97,19 +126,48 @@ function Chat () {
 		}
 	}
 
-	function handleSendMessage() {
-		if (!socket || receiverId === -1) return
-
-		socket.emit('sendMessage', {
-			content: message,
-			receiverId: receiverId,
+	async function handleOpenNotifications() {
+		const res = await fetch('/api/friend/requests', {
+			method:"GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			}	
 		})
-		setMessage("")
+		if (!res.ok)
+			throw new Error("Unauthorized token.")
+		const data = await res.json()
+		setNotifications(
+			data.map((req : any)=>({
+				id: req.id,
+				fromUserId: req.userId,
+				fromName: req.user.name,
+				status: req.status,
+			})
+		))
+		setOpenNotifications(true)
 	}
-	
+
+	async function handleInteractionWithFriend(friendId: number) {
+		if (friendId === -1) return 
+		setReceiverId(friendId)
+		const res = await fetch(`/api/messages?userId=${myId}&friendId=${friendId}`, {
+			method: 'GET',
+			headers : {Authorization: `Bearer ${token}`}
+		})
+		if (!res.ok)
+			throw new Error("Unauthorized token.")
+		const data = await res.json()
+		setMessages(data)
+	}
+
+
 	return(
 		<div>
-			<h2>Chat</h2>
+			<div>
+				<h2>Chat</h2>
+				<button onClick={handleOpenNotifications}>Notifications</button>
+				{openNotifications && <Notifications notifications={notifications} setNotifications={setNotifications} setFriendsVersion={setFriendsVersion} />}
+			</div>
 			<form onSubmit={handleSearchRequest}>
 				<div>
 					<label htmlFor="search_one"></label>
@@ -129,9 +187,9 @@ function Chat () {
 			<ChatStyle>
 				<div className="left">
 					<h3>Friends</h3>
-					{friends.map((friend)=><div key={friend.friend.friendId}>
+					{friends.map((friend)=><div key={friend.id}>
 						<button
-							onClick={()=>setReceiverId(friend.friend.id)}>
+							onClick={()=>handleInteractionWithFriend(friend.friend.id)}>
 							{friend.friend.name}
 						</button>
 					</div>)}
@@ -150,6 +208,60 @@ function Chat () {
 					<button onClick={handleSendMessage}>Send</button>
 				</div>
 			</ChatStyle>
+		</div>
+	)
+}
+
+type NotificationsProps = {
+	notifications: Notification[];
+	setNotifications: React.Dispatch<React.SetStateAction<Notification[]>>;
+	setFriendsVersion: React.Dispatch<React.SetStateAction<number>>
+}
+
+function Notifications({notifications, setNotifications, setFriendsVersion} : NotificationsProps) {
+	const token = localStorage.getItem("access_token")
+
+	async function acceptFriend(notif: Notification) {
+		const res = await fetch(`/api/friend/accept/${notif.fromUserId}`, {
+			method: 'POST',
+			headers: {
+				"Content-type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({
+				friendId: notif.fromUserId,
+			})
+		})
+		if (!res.ok)
+			throw new Error(`${res.status}: ${res.statusText}`)
+		setNotifications(prev=>prev.filter(n=>n.fromUserId !== notif.fromUserId))
+		setFriendsVersion(prev=>prev + 1)
+	}
+
+	async function refuseFriend(notif: Notification) {
+		const res = await fetch(`/api/friend/refuse/${notif.fromUserId}`, {
+			method: 'POST',
+			headers: {Authorization: `Bearer ${token}`},
+		})
+		if (!res.ok)
+			throw new Error(`${res.status}: ${res.statusText}`)
+		setNotifications(prev=>prev.filter(n=>n.fromUserId !== notif.fromUserId))
+	}
+
+	return (
+		<div>
+			<ul>
+				{notifications.map((notif, i)=><div key={i}>
+					<p>{notif.fromName} ask to be friend.</p>
+					{
+						notif.status === 'PENDING' && 
+						<>
+							<button onClick={()=>acceptFriend(notif)}>Accept</button>
+							<button onClick={()=>refuseFriend(notif)}>Refuse</button>
+						</>
+					}
+				</div>)}
+			</ul>
 		</div>
 	)
 }
